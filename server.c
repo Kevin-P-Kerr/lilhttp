b07fef64c250014e3163aaa9a030e58f1d2b78dd
Kevin <kkerr87@gmail.com>
2012-04-11 18:03:11 -0400

    Refactored code for clarity and error handling

--- server.c
+++ server.c
@@ -7,51 +7,67 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-char ttyin(void) {
-	FILE *tty;
-	char nc;
-	if ((tty=fopen("/dev/tty", "r"))==NULL) {
-		fprintf(stderr, "ttyin error\n");
+int CreateSocket(int *fd) {
+	*fd = socket(AF_INET, SOCK_STREAM, 0);
+	return 1;
+};
+
+int Bind(int *fd, struct sockaddr_in *skaddr) {
+	if (bind(*fd, (struct sockaddr *) skaddr, sizeof(*skaddr))<0) {
+		fprintf(stderr, "BIND ERROR\n");
 		exit(EXIT_FAILURE);
-	} if ((nc=fgetc(tty))==EOF) { // nothing happened
-		return 'c';
 	} else {
-		return nc;
+		return 1;
+	}
+};
+
+int Accept(int *nsfd, int *sfd, struct sockaddr_in *cli_addr, int *clilen) {
+	if ((*nsfd = accept(*sfd, (struct sockaddr *) cli_addr, clilen))<0) {
+		fprintf(stderr, "ACCEPT ERROR\n");
+		exit(EXIT_FAILURE);
+	} else {
+		return 1;
+	}
+};
+
+int Read(int *fd, char *buf, int buf_siz) {
+	if (read(*fd, buf, buf_siz)<0) {
+		fprintf(stderr, "Read Error\n");
+		exit(EXIT_FAILURE);
+	}else {
+		return 1;
+	}
+};
+
+int Write(int *fd, char *msg, int len) {
+	if (write(*fd, msg, len)<0) {
+		fprintf(stderr, "Write Error\n");
+		exit(EXIT_FAILURE);
+	} else {
+		return 1;
 	}
 };
 
 int main(int argc, char *argv[]) {
-	char nc, *progname=argv[0];
+	char *progname=argv[0];
 	int sockfd, newsockfd, portno, clilen, n;
 	char buffer[BUFSIZ];
 	struct sockaddr_in serv_addr, cli_addr;
 	if (argc < 2 ) {
 		fprintf(stderr, "\nERROR: No Port Provided\n");
 		exit(EXIT_FAILURE);
-	}if ((sockfd = socket(AF_INET, SOCK_STREAM, 0))<0) {
-		fprintf(stderr, "\nERROR: %s Could Not Open Socket\n", progname);
+	} if(!CreateSocket(&sockfd)) {
+		fprintf(stderr, "%s ERROR, COULD NOT CREATE SERVER SOCKET\n", progname);
 		exit(EXIT_FAILURE);
-	}bzero((char *) &serv_addr, sizeof(serv_addr));
-	portno = atoi(argv[1]);
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(portno);
+	} serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(atoi(argv[1]));
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr))<0) {
-		fprintf(stderr, "ERROR: %s Error On Bindings\n", progname);
-		exit(EXIT_FAILURE);
-	} listen(sockfd, 5);
+	Bind(&sockfd, &serv_addr);
+	listen(sockfd, 5);
 	clilen = sizeof(cli_addr);
-	if ((newsockfd=accept(sockfd, (struct sockaddr *) &cli_addr, &clilen))<0){
-		fprintf(stderr, "%s: ERROR ON ACCEPT\n", progname);
-		exit(EXIT_FAILURE);
-	} bzero(buffer, BUFSIZ);
-	if ((n=read(newsockfd, buffer, BUFSIZ))<0) {
-		fprintf(stderr, "%s, READ ERROR\n", progname);
-		exit(EXIT_FAILURE);
-	}printf("Here is the message %s\n", buffer);
-	n = write(newsockfd, "I got your message", 18);
-	if (n<0) {
-		fprintf(stderr, "%s, ERROR WRITING TO SOCKET", progname);
-		exit(EXIT_FAILURE);
-	}exit(EXIT_SUCCESS);
+	Accept(&newsockfd, &sockfd, &cli_addr, &clilen);
+	bzero(buffer, BUFSIZ); // reset buffer
+	Read(&newsockfd, buffer, BUFSIZ);
+	Write(&newsockfd, "I got your message!", 18);
+	exit(EXIT_SUCCESS);
 }; 
