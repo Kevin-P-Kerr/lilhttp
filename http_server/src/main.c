f64f0a613c80eba3fc8eb474a4e0044b47e1c837
Kevin <kkerr87@gmail.com>
2012-05-14 14:58:54 -0400

    Removed old Makefile

--- http_server/src/main.c
+++ http_server/src/main.c
@@ -1,17 +1,14 @@
 // A simple web  server //
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/types.h>
-#include <sys/socket.h>
+#include <errno.h>
+#include <sys/epoll.h>
 #include <sys/stat.h>
 #include <netinet/in.h>
 #include <time.h>
-#include <sys/epoll.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <errno.h>
 #include "header.h"
 
 int main(int argc, char *argv[]) {
